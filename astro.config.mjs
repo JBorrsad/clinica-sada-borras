197c2da71bb06fa59aead137a2f7f4276dce08fc
JBorrsad <149406696+JBorrsad@users.noreply.github.com>
2025-10-15 06:38:04 +0200

    implantologia, sobre nosotros.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://jborrsad.github.io",
-  base: "/clinica-sada-borras",
+  // base: "/clinica-sada-borras", // Comentado para desarrollo local
   compressHTML: true,
   build: {
     inlineStylesheets: "auto",
