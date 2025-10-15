87ecc55b5bf353e37d216e9fa3e28a7ee9b74029
JBorrsad <149406696+JBorrsad@users.noreply.github.com>
2025-10-15 06:46:07 +0200

    -

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://jborrsad.github.io",
-  // base: "/clinica-sada-borras", // Comentado para desarrollo local
+  base: "/clinica-sada-borras",
   compressHTML: true,
   build: {
     inlineStylesheets: "auto",
